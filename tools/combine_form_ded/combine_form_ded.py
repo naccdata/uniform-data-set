b3d533911076d2747a7c7119f9b3108de10c0fd1
Emily Cheng <echeng06@uw.edu>
2025-01-24 16:31:11 -0800

    Refactor DED generation script (#354)
    * Add preprocessing check for invalid module
    
    * Remove extra column
    
    * Update script to generate DED for FVP
    
    * Fix newline issue on B1l
    
    * Convert LBD to UTF-8
    
    * Add UTF-8 conversion script
    
    * Fix weird characters in E2F
    
    * Fix double quote
    
    * Fix B1L again
    
    * Fix apostrophe
    
    * Need to fix script
    
    * Save state
    
    * Refactor DED generation script
    
    * Formatting

--- tools/combine_form_ded/combine_form_ded.py
+++ tools/combine_form_ded/combine_form_ded.py
@@ -1,16 +1,17 @@
 """
 Generates the DED for a specific module. To use,
-update `root_directory` and `output_filename` to the
-desired values, e.g. for root_directory
+scroll to the bottom __main__ and update the
+`module`, `visit`, and `output_filename` variables.
 
-UDS: ../../forms/uds
-FTLD: ../../forms/ftld
-3.0 LBD: ../../forms/lbd/long
-3.1 LBD: ../../forms/lbd/short
+EXTRA STEPS:
 
-NOTE: For LBD, it is easiest to copy the `header`
-directory to the long and short directories as well (just
-don't check it in).
+1) For LBD, you need to copy the `header` directory to
+   the long and short directories as well (just don't
+   check it in).
+2) For FVP, you need to manually replace the packets
+   since most will pull from the IVP packet
+   e.g. in the raw CSV, ctrl-f for `,IF,` and replace with `,FF,`
+        note the commas to avoid accidentally replacing something unrelated
 """
 import logging
 import os
@@ -21,7 +22,19 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
-def ensure_directory_exists(file_path):
+class VisitType:
+    IVP = 'ivp'
+    FVP = 'fvp'
+
+
+class ModuleType:
+    UDS = 'uds'
+    FTLD = 'ftld'
+    LBD_LONG = 'lbd/long'
+    LBD_SHORT = 'lbd/short'
+
+
+def ensure_directory_exists(file_path: str) -> None:
     # Get the directory name from the file path
     directory = os.path.dirname(file_path)
     # Create the directory if it does not exist
@@ -29,17 +42,46 @@ def ensure_directory_exists(file_path):
         os.makedirs(directory)
 
 
-def clean_newlines(value):
+def clean_newlines(value: str) -> str:
     # Replace newline characters with an empty string
     if isinstance(value, str):
         return value.replace('\n', '').replace('\r', '')
     return value
 
 
-def concat_form(subdir, file, combined_df, header_df, qnv_found: bool = False):
-     # Check if the filename matches the pattern
-    if 'form_' in file and '_questions_' in file and '_ivp_' in file and file.endswith('.csv'):
-        qnv_found = True
+class DedGenerator:
+    """Class for generating the DED."""
+
+    def __init__(self, root_dir: str, module: str, visit: str):
+        """Initialize empty DFs"""
+        self.__root_dir = f'{root_dir}/{module}'
+        self.__module = module
+        self.__visit = visit
+
+        self.__combined_df = pd.DataFrame(dtype=object)
+        self.__header_df = None
+
+    def concat_form(self, subdir: str, file: str, qnv_found: bool, override_visit: str = None) -> bool:
+        """If a valid Q&V CSV for the visit, concat to the DED
+
+        Args:
+            subdir: The subdirectory containing this file
+            file: The file to maybe add to the DED
+            qnv_found: Whether or not we've found a Q&V for this form yet
+            override_visit: The override visit
+
+        Returns:
+            True if the Q&V was found, False otherwise
+        """
+        visit = override_visit if override_visit else self.__visit
+        # Check if the filename matches the pattern
+        if (not file.endswith('.csv')        # not a CSV
+            or not file.startswith('form_')  # not a form CSV
+            or '_questions_' not in file     # not a Q&V CSV
+            or f"_{visit}_" not in file):    # not a visit we care about
+            return qnv_found                 # return whatever the previous state was
+
+        # found the form, will always return True after this point
         try:
             file_path = os.path.join(subdir, file)
             log.info(f"Adding {file_path}")
@@ -53,45 +95,123 @@ def concat_form(subdir, file, combined_df, header_df, qnv_found: bool = False):
             df = df.map(clean_newlines)
             # Append the data to the combined DataFrame
             if 'header' in file:
-                return qnv_found, combined_df, df
+                log.info(f"Found header file: {file}")
+                if self.__header_df:
+                    raise ValueError("Multiple header DFs found")
+
+                self.__header_df = df
+                return True
             else:
-                combined_df = pd.concat([combined_df, df], ignore_index=True)
+                self.__combined_df = pd.concat([self.__combined_df, df], ignore_index=True)
         except Exception as e:
-            log.warning(f'File {file_path} threw an exception: {e}')   
+            log.warning(f'File {file_path} threw an exception: {e}')
 
-    return qnv_found, combined_df, header_df
+        return True
 
+    def handle_lbd(self, subdir: str, qnv_found: bool) -> bool:
+        """Handle LBD, which needs to handle both the long and short case.
 
-def main(root_directory, output_filename):
-    # Initialize an empty DataFrame
-    combined_df = pd.DataFrame(dtype=object)
-    header_df = None
+        Args:
+            subdir: The subdirectory to search in
+            qnv_found: Whether or not we've found a Q&V for this form yet
+        Returns:
+            Whether or not the Q&V was found
+        """
+        long_subdir = subdir.replace('short', 'long')
+        form = subdir.split('/')[-1].strip()
 
-    # Walk through the directory
-    for subdir, dirs, files in os.walk(root_directory):
-        dirs.sort()  # ensures order
-        qnv_found = False
-        for file in files:
-            qnv_found, combined_df, header_df = \
-                concat_form(subdir, file, combined_df, header_df, qnv_found)
+        # LBD short does not have B8l, skip it by returning true so it doesn't
+        # trigger other searches
+        if self.__module == ModuleType.LBD_SHORT and form == 'b8l':
+            return True
 
-        # for LBD, get from long directory if not found
-        if not qnv_found:
-            long_subdir = subdir.replace('short', 'long')
+        # 3. For LBD Short (3.1) FVP, grab from either LBD Short IVP OR LBD Long FVP
+        if self.__visit == VisitType.FVP:
+            # LBD Short FVP specific case - these will try to pull from LBD Long FVP then LBD Long IVP
+            if self.__module == ModuleType.LBD_SHORT:
+                if form in ['b3l', 'b5l', 'b7l', 'd1l', 'e1l', 'header']:
+                    for file in os.listdir(long_subdir):
+                        qnv_found = self.concat_form(long_subdir, file, qnv_found,
+                                                     override_visit=VisitType.FVP)
+                    if not qnv_found:
+                        for file in os.listdir(long_subdir):
+                            qnv_found = self.concat_form(long_subdir, file, qnv_found,
+                                                         override_visit=VisitType.IVP)
+
+            # 1. In general, for any FVP, grab from the IVP directory (this case is for LBD Long FVP)
+            if not qnv_found:
+                for file in os.listdir(subdir):
+                    qnv_found = self.concat_form(subdir, file, qnv_found,
+                                                 override_visit=VisitType.IVP)
+
+        # 2. For LBD Short (3.0) IVP, grab from LBD Long IVP
+        elif self.__module == ModuleType.LBD_SHORT:
             for file in os.listdir(long_subdir):
-                _, combined_df, header_df = \
-                    concat_form(long_subdir, file, combined_df, header_df)
+                qnv_found = self.concat_form(long_subdir, file, qnv_found)
 
-    if header_df is not None:
-        combined_df = pd.concat([header_df, combined_df], ignore_index=True)
+        # LBD Long IVP should have had its own Q&V file, so we're done here
+        return qnv_found
 
-    # Ensure the output directory exists
-    ensure_directory_exists(output_filename)
-    # Export the combined DataFrame to a CSV file
-    combined_df.to_csv(output_filename, index=False, quoting=csv.QUOTE_MINIMAL)
-    log.info(f"Combined CSV file saved as {output_filename}")
+    def generate(self, output_filename: str) -> None:
+        """Generate the DED.
+
+        Args:
+            output_filename: Name of the output file to write results to
+        """
+        # Walk through the directory
+        for subdir, dirs, files in os.walk(self.__root_dir):
+            dirs.sort()  # ensures order
+            qnv_found = False
+
+            # see if the visit Q&V is defined for this module to begin with
+            for file in files:
+                qnv_found = self.concat_form(subdir, file, qnv_found)
+
+            if qnv_found:
+                continue
+
+            # If not, we have to grab from a corresponding visit, e.g.
+            #   1. In general, for any FVP, grab from the IVP directory
+            #   2. For LBD Short (3.0) IVP, grab from LBD Long IVP
+            #   3. For LBD Short (3.1) FVP, grab from either LBD Short IVP OR for special cases:
+            #       - Look for LBD Long FVP
+            #       - If not found, look at LBD Long IVP
+
+            # Handle the LBD cases
+            if self.__module in [ModuleType.LBD_LONG, ModuleType.LBD_SHORT]:
+                qnv_found = self.handle_lbd(subdir, qnv_found)
+
+            # everything else should be an FVP grabbing from the IVP directory; this is
+            # the last case so we don't care about the result
+            if not qnv_found:
+                for file in os.listdir(subdir):
+                    self.concat_form(subdir, file, qnv_found, override_visit=VisitType.IVP)
+
+        # Concat header to beginning of the DF
+        if self.__header_df is None:
+            raise ValueError(f"No header file found for {self.__module}")
+        self.__combined_df = pd.concat([self.__header_df, self.__combined_df], ignore_index=True)
+
+        # Ensure the output directory exists
+        ensure_directory_exists(output_filename)
+
+        # Export the combined DataFrame to a CSV file
+        self.__combined_df.to_csv(output_filename, index=False, quoting=csv.QUOTE_MINIMAL)
+        log.info(f"Combined CSV file saved as {output_filename}")
+
 
 if __name__ == "__main__":
-    root_directory = '../../forms/lbd/short'
-    output_filename = './combined_ded/3.1_lbd_ded.csv'
-    main(root_directory, output_filename)
+
+    # CHANGE THESE
+    # If LBD, make sure the LBD long/short directories each have
+    # their own copy of the header files before running
+    module = ModuleType.LBD_SHORT
+    visit = VisitType.FVP
+    output_filename = './combined_ded/output.csv'
+
+    generator = DedGenerator(
+        root_dir='../../forms',
+        module=module,
+        visit=visit)
+
+    generator.generate(output_filename)
---
+++ tools/combine_form_ded/convert_to_utf8.py
@@ -0,0 +1,33 @@
+"""
+Some of the CSVs are encoded in cp1252 which causes issues downstream,
+this script converts them to utf-8.
+"""
+
+import logging
+from pathlib import Path
+
+log = logging.getLogger(__name__)
+
+def convert_to_utf8(file: Path) -> None:
+    """Convert a file to UTF-8 if a different format.
+    Really only have to worry about CP-1252 so just try-catch.
+
+    NOTE: THIS WILL OVERWRITE THE SOURCE FILE IN-PLACE IF NOT UTF-8
+    """
+    try:
+        with file.open('r', encoding='utf-8') as fh:
+            fh.read()
+    except UnicodeDecodeError as e:
+        log.warning(f"{file} is not UTF-8 - converting")
+        contents = None
+        with file.open('r', encoding='cp1252') as fh:
+            contents = fh.read()
+
+        with file.open('w', encoding='utf-8') as fh:
+            fh.write(contents)
+
+FORMS_DIR = Path('../../forms')
+
+# ADD/ITERATE OVER WHICHEVER FILES YOU NEED TO CONVERT
+convert_to_utf8(FORMS_DIR / 'lbd/long/e2l/form_e2l_fvp_questions_and_vars.csv')
+convert_to_utf8(FORMS_DIR / 'lbd/long/e3l/form_e3l_fvp_questions_and_vars.csv')
